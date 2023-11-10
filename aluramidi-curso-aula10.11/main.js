4d4e4d72c2023e30a3d3176b387d888b1a06fc64
jujubaalura2007 <126872805+jujubaalura2007@users.noreply.github.com>
2023-11-10 08:27:13 -0300

    curso finalizado

---
+++ aluramidi-curso-aula10.11/main.js
@@ -0,0 +1,39 @@
+function tocaSom (seletorAuio) {
+    const elemento=document.querySelectorAll (seletorAudio);
+    if(elemnto |=null && elemento.localName === 'audio') {
+        elemento.play();
+    }
+else{
+    console.log('Elemento não encontrado ou seletor inválido');
+}
+}
+listaDeTeclas = document.querySelectorAll('.tecla');
+let contador = 0;
+
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];  
+    const idAudio = `#som_${instrumento}`; //TEMPLATE STRING
+ 
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+
+    }
+    
+    tecla.onkeydown = function(evento) {
+
+        console.log(evento.code === 'Space')
+
+    if(evento.code ==='Space'|| evento.code === 'Enter'){
+        tecla.classList.add('ativa');
+    }
+
+    }
+
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa');
+    
+    }
+} 
